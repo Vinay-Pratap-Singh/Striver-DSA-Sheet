28d5732d3a4ea2beee2185a601f869e296144705
Vinay-Pratap-Singh <technicalharvi@gmail.com>
2022-06-20 13:20:40 +0530

    19 Next permutation
    19th solution submitted

---
+++ 19_Next_permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int breakpoint = -1;
+        for(int i=nums.size()-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                breakpoint = i-1;
+                break;
+            }
+        }
+        
+        if(breakpoint < 0){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] > nums[breakpoint]){
+                swap(nums[breakpoint], nums[i]);
+                reverse(nums.begin()+breakpoint+1, nums.end());
+                break;
+            }
+        }
+    }
+};
